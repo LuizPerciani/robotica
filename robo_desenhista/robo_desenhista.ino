b5d88b21be84713ce30ee9053528acb8c70b0a75
luiz.glperciani1 <lgperciani@gmail.com>
2018-09-11 17:11:28 -0300

    modifiquei o arquivo

--- robo_desenhista/robo_desenhista.ino
+++ robo_desenhista/robo_desenhista.ino
@@ -1,14 +1,14 @@
-/**
- * robô desenhista
- * @author Luiz Guilherme Lopes Perciani
- * Data 11/09/2018
- */
+
 
 
 
 
 void setup() {
-  // 
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (9, OUTPUT);
+  pinMode (10, OUTPUT);
+ 
 
 }
 
